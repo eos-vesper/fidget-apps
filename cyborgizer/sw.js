552cc3ead0fe0dbdfd92dc1a7ca39c88120ce3ff
eos-vesper <kamil666baranski@gmail.com>
2026-03-14 11:45:58 +0100

    Add files via upload

---
+++ cyborgizer/sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'cyborgizer-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg'
+];
+
+// Instalacja i buforowanie zasobów
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Aktywacja i czyszczenie starych wersji cache
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Obsługa żądań sieciowych
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
